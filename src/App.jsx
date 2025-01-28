a79f66b4eb1b79474635d6a4b58d23487f003d1d
Wintorez7 <liordx76@gmail.com>
2025-01-28 13:42:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+ 
+  return (
+    <main>
+       <div className='pattern'>
+          <div className='wrapper'>
+              <header>
+                <h1>Find Movies You'll Enjoy without the Hussel</h1>
+              </header>
+          </div>
+       </div>
+    </main>
+  )
+}
+
+export default App
