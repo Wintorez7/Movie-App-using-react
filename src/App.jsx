aeaf15cdca9a3ba298fbbdb44c3aa2691020a9f5
Wintorez7 <liordx76@gmail.com>
2025-01-29 23:17:54 +0530

    adding spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
-import Search from './components/Search'
+import Search from './components/Search.jsx'
 
 const API_BASE_URL = "https://api.themoviedb.org/3"; 
-const API_KEY = "YOUR_API_KEY_HERE"; 
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY; 
 
 const API_OPTIONS = {
+  method:'GET', 
   headers: {
     accept: 'application/json',
     Authorization: `Bearer ${API_KEY}`,
-  },
-};
+  }
+}
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -18,6 +19,8 @@ function App() {
   const [isloading, setIsLoading] = useState(false);
 
   const fetchMovie = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
@@ -28,14 +31,20 @@ function App() {
       }
 
       const data = await response.json();
+      console.log(data);
+
       if (data.Response === 'false') {
         setErrorMessage(data.Error || 'Failed to fetch movie');
-      } else {
-        setMovieList(data.results || []);
-      }
+        setMovieList([]);
+        return;
+      } 
+      setMovieList(data.results || []);
+    
     } catch (error) {
       console.log(`Error Fetching Movie: ${error}`);
       setErrorMessage('Error fetching movie. Please try again later.');
+    } finally{
+      setIsLoading(false);
     }
   };
 
@@ -58,7 +67,19 @@ function App() {
 
           <section className="all-movies">
             <h2>All Movies</h2>
-            {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+
+            {isloading ?  (
+              <p className='text-white'>Loading...</p>
+            ): errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <p key={movie.id} className='text-white'>{movie.title}</p>
+                  
+                ))}
+              </ul>
+            )}
           </section>
         </div>
       </div>
