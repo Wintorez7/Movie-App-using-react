ed01ed8561fb149afa1786b0a55857631dd94161
Wintorez7 <liordx76@gmail.com>
2025-01-30 00:56:03 +0530

    implementing searching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx';
+import MovieCard from './components/MovieCard.jsx';
 
 const API_BASE_URL = "https://api.themoviedb.org/3"; 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY; 
@@ -53,7 +55,7 @@ function App() {
   }, []);
 
   return (
-    <main>
+    <main className='overflow-x-hidden'>
       <div className="pattern">
         <div className="wrapper">
           <header>
@@ -66,16 +68,16 @@ function App() {
           </header>
 
           <section className="all-movies">
-            <h2>All Movies</h2>
+            <h2 className='mt-[20px]'>All Movies</h2>
 
             {isloading ?  (
-              <p className='text-white'>Loading...</p>
+              <Spinner/>
             ): errorMessage ? (
               <p className='text-red-500'>{errorMessage}</p>
             ) : (
               <ul>
                 {movieList.map((movie) => (
-                  <p key={movie.id} className='text-white'>{movie.title}</p>
+                  <MovieCard key={movie.id} movie={movie}/>
                   
                 ))}
               </ul>
